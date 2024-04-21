b384111f22c0093908d9c9db8e73d18d210c8f3c
MARTIN Nolann <nolann.martin.2003@gmail.com>
2024-04-21 21:46:12 +0200

    module 1 + 2

--- 1A/S6/télécom/src/TP.m
+++ 1A/S6/télécom/src/TP.m
@@ -6,52 +6,99 @@ n=1;
 M = 2^n;
 Ts = log2(M)*Tb;
 Rs = Rb/log2(M);
-Ns = Ts/Te;
 nb_bits = 100;
-
-t = linspace(0,nb_bits * Ts-Te,nb_bits);
-
-
-%modulateur 1 :
-S = randi([0 1],1,nb_bits);
-SE = 2*S-1;
+S = randi([0 1],nb_bits,1);
 
 
 
+%% modulateur 1 :
+% Mapping
 
-ht = ones(1,Ns);
-pDirac = kron(SE,[1 zeros(1,Ns-1)]);
-y = filter(ht,1,pDirac);
-dsp = fft(SE,1024);
+Ns = Fe * Ts; % Nombre d'échantillons par bits
 
-figure
-ech_freq = linspace(0,Fe,length(dsp));
+SE = (2*S - 1)';
+At = kron(SE, [1, zeros(1, Ns-1)]);
 
-plot(ech_freq,dsp);
-grid
-xlabel('Temps (s)')
-ylabel('signal')
-title('Modulateur 1');
+% Filtre
+T1 = 0:Te:(nb_bits*Ns-1)*Te; % Echelle temporelle
+h1 = ones(1, Ns); % Reponse impulsionnelle du filtre
+y = filter(h1, 1, At);
 
-%modulateur 2 :
-S2 = randi([0 1],1,nb_bits);
-S2 = reshape(S2,2,nb_bits/2);
+% Tracés
+figure('name', 'Modulateur 1')
+
+    % Signal généré
+    nexttile
+    stem(T1,At)
+    ylim([-1.5, 1.5])
+    xlabel("temps (s)")
+    ylabel("Signal temporel")
+    title('Tracé du signal temporel');
+    
+    nexttile
+    plot(T1,y)
+    ylim([-1.5, 1.5])
+    xlabel("temps (s)")
+    ylabel("Signal filtré")
+    title('Tracé du signal temporel filtré')
+    
+    % Tracer la DSP par rapport à l'axe des fréquences
+    DSP = abs(fft(y,1024));
+    axe_frequences = linspace(0, Fe, length(DSP));
+    nexttile
+    stem(axe_frequences,DSP)
+    xlabel('Fréquence (Hz)');
+    ylabel('DSP');
+    title('tracé de la DSP par rapport a la fréquence');
+
+
+%% modulateur 2 :
+% Mapping
+Ns = Fe * 2*Ts; % Nombre d'échantillons par bits
+
+S2 = reshape(S',2,nb_bits/2);
 S2E = [1, nb_bits/2];
 Choix = [-3 -1; 1 3];
 for i=1:size(S2,2)
     S2E(i) = Choix(S2(1,i)+1,S2(2,i)+1);
 end
+At = kron(S2E, [1, zeros(1, Ns-1)]);
 
+% Filtre
+T2 = 0:Te:(nb_bits*Ns-1)*Te/2; % Echelle temporelle
+h2 = ones(1, Ns); % Reponse impulsionnelle du filtre
+y = filter(h2, 1, At);
 
+% Tracés
+figure('name', 'Modulateur 2')
 
-dsp = fft(S2E,1024);
+    % Signal généré
+    nexttile
+    stem(T2,At)
+    ylim([-4,4])
+    xlabel("temps (s)")
+    ylabel("Signal temporel")
+    title('Tracé du signal temporel');
+    
+    nexttile
+    plot(T2,y)
+    ylim([-4,4])
+    xlabel("temps (s)")
+    ylabel("Signal filtré")
+    title('Tracé du signal temporel filtré')
+    
+    % Tracer la DSP par rapport à l'axe des fréquences
+    DSP = abs(fft(y,1024));
+    axe_frequences = linspace(0, Fe, length(DSP));
+    nexttile
+    stem(axe_frequences,DSP)
+    xlabel('Fréquence (Hz)');
+    ylabel('DSP');
+    title('tracé de la DSP par rapport a la fréquence');
+%% modulateur 3
+% Mapping
 
-figure
-ech_freq = linspace(0,Fe,length(dsp));
-plot(ech_freq,dsp);
-grid
-xlabel('Temps (s)')
-ylabel('signal')
-title('Modulateur 2');
+Ns = Fe * Ts; % Nombre d'échantillons par bits
 
-
+SE = (2*S - 1)';
+At = kron(SE, [1, zeros(1, Ns-1)]);
