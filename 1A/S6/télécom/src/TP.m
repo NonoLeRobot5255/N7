fc73732295de624a9300ad344e88ac3f4bd92872
MARTIN Nolann <nolann.martin.2003@gmail.com>
2024-04-22 21:25:36 +0200

    fin TP1

--- 1A/S6/télécom/src/TP.m
+++ 1A/S6/télécom/src/TP.m
@@ -6,7 +6,7 @@ n=1;
 M = 2^n;
 Ts = log2(M)*Tb;
 Rs = Rb/log2(M);
-nb_bits = 100;
+nb_bits = 150;
 S = randi([0 1],nb_bits,1);
 
 
@@ -14,10 +14,10 @@ S = randi([0 1],nb_bits,1);
 %% modulateur 1 :
 % Mapping
 
-Ns = Fe * Ts; % Nombre d'échantillons par bits
+Ns = Fe * Ts; % Nombre d'échantillons! par bits
 
 SE = (2*S - 1)';
-At = kron(SE, [1, zeros(1, Ns-1)]);
+At = kron(SE, [1 zeros(1, Ns-1)]);
 
 % Filtre
 T1 = 0:Te:(nb_bits*Ns-1)*Te; % Echelle temporelle
@@ -43,10 +43,10 @@ figure('name', 'Modulateur 1')
     title('Tracé du signal temporel filtré')
     
     % Tracer la DSP par rapport à l'axe des fréquences
-    DSP = abs(fft(y,1024));
-    axe_frequences = linspace(0, Fe, length(DSP));
+    DSP1 = pwelch(y, [],[],Fe,'twosided');
+    axe_frequences = linspace(-Fe/2, Fe/2, length(DSP1));
     nexttile
-    stem(axe_frequences,DSP)
+    semilogy(axe_frequences,fftshift(DSP1))
     xlabel('Fréquence (Hz)');
     ylabel('DSP');
     title('tracé de la DSP par rapport a la fréquence');
@@ -88,10 +88,10 @@ figure('name', 'Modulateur 2')
     title('Tracé du signal temporel filtré')
     
     % Tracer la DSP par rapport à l'axe des fréquences
-    DSP = abs(fft(y,1024));
-    axe_frequences = linspace(0, Fe, length(DSP));
+    DSP2 = pwelch(y, [],[],Fe,'twosided');
+    axe_frequences = linspace(-Fe/2, Fe/2, length(DSP2));
     nexttile
-    stem(axe_frequences,DSP)
+    semilogy(axe_frequences,fftshift(DSP2))
     xlabel('Fréquence (Hz)');
     ylabel('DSP');
     title('tracé de la DSP par rapport a la fréquence');
@@ -102,3 +102,51 @@ Ns = Fe * Ts; % Nombre d'échantillons par bits
 
 SE = (2*S - 1)';
 At = kron(SE, [1, zeros(1, Ns-1)]);
+
+% Filtre
+T3 = 0:Te:(nb_bits*Ns-1)*Te; % Echelle temporelle
+h3= rcosdesign(0.5,nb_bits/Ns,Ns);
+y = filter(h3,1,At);
+
+% Tracés
+figure('name', 'Modulateur 3')
+
+    % Signal généré
+    nexttile
+    stem(T1,At)
+    ylim([-1.5, 1.5])
+    xlabel("temps (s)")
+    ylabel("Signal temporel")
+    title('Tracé du signal temporel');
+    
+    nexttile
+    plot(T1,y)
+    ylim([-1.5, 1.5])
+    xlabel("temps (s)")
+    ylabel("Signal filtré")
+    title('Tracé du signal temporel filtré')
+    
+    % Tracer la DSP par rapport à l'axe des fréquences
+    DSP3 = pwelch(y, [],[],Fe,'twosided');
+    axe_frequences = linspace(-Fe/2, Fe/2, length(DSP3));
+    nexttile
+    semilogy(axe_frequences,fftshift(DSP))
+    xlabel('Fréquence (Hz)');
+    ylabel('DSP');
+    title('tracé de la DSP par rapport a la fréquence');
+%% tracé des DSP sur le même graphique 
+
+figure('Name','Superposition des DSP')
+
+semilogy(axe_frequences, fftshift(DSP1), 'DisplayName', 'Modulateur 1');
+hold on; % Permet de garder le graphique actuel et d'ajouter d'autres courbes
+semilogy(axe_frequences, fftshift(DSP2), 'DisplayName', 'Modulateur 2');
+semilogy(axe_frequences, fftshift(DSP3), 'DisplayName', 'Modulateur 3');
+
+xlabel('Fréquence (Hz)')
+ylabel('DSP')
+title('Tracé des DSP en superposition')
+
+legend('show'); % Afficher la légende
+
+%% TP 2 
