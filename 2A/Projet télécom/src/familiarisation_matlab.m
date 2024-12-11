d71391863ee979f2421973459780982f4ac08b97
MARTIN Nolann <nolann.martin.2003@gmail.com>
2024-12-11 09:46:08 +0100

    codage sof et hard san poinçonnage ok

--- 2A/Projet télécom/src/familiarisation_matlab.m
+++ 2A/Projet télécom/src/familiarisation_matlab.m
@@ -1,2 +1,25 @@
+%% on efface tout et set les variables
+clear all;
+close all;
+
+nb_bits = 2000;
+
+%% tracé du treillis 
 trellis = poly2trellis(3,[5 7]);
 commcnv_plotnextstates(trellis.outputs);
+
+%% code convolutif 
+% signal crée
+S = randi([0 1],1,nb_bits);
+
+% codage canal
+code_codage=convenc(S,trellis);
+
+matrice_comparaison = [S ones(size(S))*2 ; code_codage];
+
+% code sorti
+
+code_decode = vitdec(code_codage,trellis,5*(3-1),'term','hard');
+
+mat = (S~=code_decode);
+TEB = mean(mat)
