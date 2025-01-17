8c768f7051185553651811e57dc30d0816df03d7
MARTIN Nolann <nolann.martin.2003@gmail.com>
2025-01-17 20:06:00 +0100

    TP intergiciel

---
+++ 2A/intergiciel/ws-subject/students-server/src/main/java/n7/students_server/StudentsServerApplication.java
@@ -0,0 +1,13 @@
+package n7.students_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsServerApplication.class, args);
+	}
+
+}
